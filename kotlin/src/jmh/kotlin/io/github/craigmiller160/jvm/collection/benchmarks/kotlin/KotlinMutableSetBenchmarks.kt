1b76a0702a7e605d711b0d4827fb5920bdd6faa0
Craig Miller <craigmiller160@gmail.com>
2023-03-18 17:08:35 -0400

    More benchmarks for kotlin

---
+++ kotlin/src/jmh/kotlin/io/github/craigmiller160/jvm/collection/benchmarks/kotlin/KotlinMutableSetBenchmarks.kt
@@ -0,0 +1,50 @@
+package io.github.craigmiller160.jvm.collection.benchmarks.kotlin
+
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.Level
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
+import org.openjdk.jmh.annotations.State
+
+class KotlinMutableSetBenchmarks {
+    @State(Scope.Benchmark)
+    class KotlinMutableSetState {
+        val SIZE: Int = System.getenv("COLLECTION_SIZE").toInt()
+
+        lateinit var SET: MutableSet<String>
+        lateinit var MORE_RECORDS: MutableSet<String>
+
+        @Setup(Level.Invocation)
+        fun setUp() {
+            SET = (0 until SIZE).map { "$it" }.toMutableSet()
+            MORE_RECORDS = (0 until 100).map { "more_$it" }.toMutableSet()
+        }
+    }
+
+    private fun validateState(state: KotlinMutableSetState) {
+        if (state.SET.size != state.SIZE) {
+            throw Error("State has invalid value: ${state.SET.size}")
+        }
+    }
+
+    @Benchmark
+    fun add1(state: KotlinMutableSetState): Set<String?>? {
+        validateState(state)
+        state.SET += "Hello"
+        return state.SET
+    }
+
+    @Benchmark
+    fun add100(state: KotlinMutableSetState): Set<String?>? {
+        validateState(state)
+        state.SET += state.MORE_RECORDS
+        return state.SET
+    }
+
+    @Benchmark
+    fun remove1(state: KotlinMutableSetState): Set<String?>? {
+        validateState(state)
+        state.SET.remove("1000")
+        return state.SET
+    }
+}
--- kotlin/src/jmh/kotlin/io/github/craigmiller160/jvm/collection/benchmarks/kotlin/KotlinSetBenchmarks.kt
+++ kotlin/src/jmh/kotlin/io/github/craigmiller160/jvm/collection/benchmarks/kotlin/KotlinSetBenchmarks.kt
@@ -1,4 +1,47 @@
 package io.github.craigmiller160.jvm.collection.benchmarks.kotlin
 
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.Level
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
+import org.openjdk.jmh.annotations.State
+
 class KotlinSetBenchmarks {
+    @State(Scope.Benchmark)
+    class KotlinSetState {
+        val SIZE: Int = System.getenv("COLLECTION_SIZE").toInt()
+
+        lateinit var SET: Set<String>
+        lateinit var MORE_RECORDS: Set<String>
+
+        @Setup(Level.Invocation)
+        fun setUp() {
+            SET = (0 until SIZE).map { "$it" }.toSet()
+            MORE_RECORDS = (0 until 100).map { "more_$it" }.toSet()
+        }
+    }
+
+    private fun validateState(state: KotlinSetState) {
+        if (state.SET.size != state.SIZE) {
+            throw Error("State has invalid value: ${state.SET.size}")
+        }
+    }
+
+    @Benchmark
+    fun add1(state: KotlinSetState): Set<String?>? {
+        validateState(state)
+        return state.SET + "Hello"
+    }
+
+    @Benchmark
+    fun add100(state: KotlinSetState): Set<String?>? {
+        validateState(state)
+        return state.SET + state.MORE_RECORDS
+    }
+
+    @Benchmark
+    fun remove1(state: KotlinSetState): Set<String?>? {
+        validateState(state)
+        return state.SET.filter { it != "1000" }.toSet()
+    }
 }
