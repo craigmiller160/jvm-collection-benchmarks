1d237b717da0122895a650489f27465dcdf15b0d
Craig Miller <craigmiller160@gmail.com>
2023-03-18 11:54:32 -0400

    Still trying to make it run

--- java/src/main/java/io/github/craigmiller160/jvm/collection/benchmarks/java/ListBenchmarks.java
+++ java/src/main/java/io/github/craigmiller160/jvm/collection/benchmarks/java/ListBenchmarks.java
@@ -1,6 +1,10 @@
 package io.github.craigmiller160.jvm.collection.benchmarks.java;
 
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.profile.GCProfiler;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.concurrent.TimeUnit;
 
@@ -8,6 +12,12 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @BenchmarkMode(Mode.AverageTime)
 public class ListBenchmarks {
+    public static void main(final String[] args) throws RunnerException {
+        final var options = new OptionsBuilder()
+                .addProfiler(GCProfiler.class) // TODO what profiler?
+                .build();
+        new Runner(options).run();
+    }
     @Benchmark
     public void hello() {
         System.out.println("Hello World");
