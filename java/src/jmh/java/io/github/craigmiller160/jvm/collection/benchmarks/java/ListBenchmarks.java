e140b210ea1ea23c636f10e07c0b3f86e62863b2
Craig Miller <craigmiller160@gmail.com>
2023-03-18 12:25:06 -0400

    Got it working

--- java/src/jmh/java/io/github/craigmiller160/jvm/collection/benchmarks/java/ListBenchmarks.java
+++ java/src/jmh/java/io/github/craigmiller160/jvm/collection/benchmarks/java/ListBenchmarks.java
@@ -14,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 public class ListBenchmarks {
     @Benchmark
     public void hello() {
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
+        String a = "hello";
     }
 }
