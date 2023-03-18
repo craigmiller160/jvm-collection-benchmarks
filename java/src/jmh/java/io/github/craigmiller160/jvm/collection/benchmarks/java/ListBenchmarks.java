922237d55f112d4b2192195e77e653fb3fa159b1
Craig Miller <craigmiller160@gmail.com>
2023-03-18 14:02:55 -0400

    Setup more list benchmarks

--- java/src/jmh/java/io/github/craigmiller160/jvm/collection/benchmarks/java/ListBenchmarks.java
+++ java/src/jmh/java/io/github/craigmiller160/jvm/collection/benchmarks/java/ListBenchmarks.java
@@ -15,6 +15,7 @@ public class ListBenchmarks {
     public static class ArrayListState {
         public static final int SIZE = Integer.parseInt(System.getenv("COLLECTION_SIZE"));
         public List<String> LIST;
+        public List<String> MORE_RECORDS;
 
         @Setup(Level.Invocation)
         public void setUp() {
@@ -22,17 +23,45 @@ public class ListBenchmarks {
             for (int i = 0; i < SIZE; i++) {
                 LIST.add(String.valueOf(i));
             }
+
+            MORE_RECORDS = new ArrayList<>();
+            for (int i = 0; i < 100; i++) {
+                MORE_RECORDS.add(String.valueOf(i));
+            }
         }
     }
 
-
-
-    @Benchmark
-    public List<String> listAdd(final ArrayListState state) {
-        if (state.LIST.size() != 2) {
+    private void validateState(final ArrayListState state) {
+        if (state.LIST.size() != ArrayListState.SIZE) {
             throw new Error("State has invalid value: %d".formatted(state.LIST.size()));
         }
+    }
+
+    @Benchmark
+    public List<String> listAppend1(final ArrayListState state) {
+        validateState(state);
         state.LIST.add("Hello");
+        return state.LIST;
+    }
+
+    @Benchmark
+    public List<String> listAppend100(final ArrayListState state) {
+        validateState(state);
+        state.LIST.addAll(state.MORE_RECORDS);
+        return state.LIST;
+    }
+
+    @Benchmark
+    public List<String> listRemove1AtEnd(final ArrayListState state) {
+        validateState(state);
+        state.LIST.remove(ArrayListState.SIZE - 1);
+        return state.LIST;
+    }
+
+    @Benchmark
+    public List<String> listRemove1AtStart(final ArrayListState state) {
+        validateState(state);
+        state.LIST.remove(0);
         return state.LIST;
     }
 }
